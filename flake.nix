c1e0ac52fceac02302f0f0d360f2e482eb12babe
Nika <nika@nikableh.moe>
2025-12-15 13:45:55 +0300

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Development environment for the project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bash
+            pkgs.deno
+            pkgs.nodejs
+            pkgs.node-gyp
+            pkgs.python3
+          ];
+        };
+      }
+    );
+}
