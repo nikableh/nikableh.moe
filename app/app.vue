983c8e5ee0f2fe9392ff8165da0cc74cfd8f22d3
Nika <nika@nikableh.moe>
2025-12-18 23:13:12 +0300

    Add WebringNavBar component

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="bg-neutral-900 h-screen text-white">
-        haiiiiiiiii
+    <div class="bg-neutral-900 h-full text-bodily-500">
+        <main class="h-screen">
+          
+        </main>
         <WebringNavBar />
     </div>
 </template>
--- app/components/WebringNavBar.vue
+++ app/components/WebringNavBar.vue
@@ -1,9 +1,23 @@
 <template>
-  <nav class="">
-    <ul>
-        <li><NuxtLink to="https://webring.otomir23.me/nikableh/prev">&lt; prev</NuxtLink></li>
-        <li><NuxtLink to="https://webring.otomir23.me/">webring</NuxtLink></li>
-        <li><NuxtLink to="https://webring.otomir23.me/nikableh/next">next &gt;</NuxtLink></li>
-      </ul>
-  </nav>
+    <nav class="">
+        <ul class="flex justify-between p-2">
+            <li>
+                <NuxtLink
+                    to="https://webring.otomir23.me/nikableh/prev"
+                    class="before:content-['< ']"
+                    >prev</NuxtLink
+                >
+            </li>
+            <li>
+                <NuxtLink to="https://webring.otomir23.me/">webring</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink
+                    to="https://webring.otomir23.me/nikableh/next"
+                    class="after:content-[' >']"
+                    >next</NuxtLink
+                >
+            </li>
+        </ul>
+    </nav>
 </template>
