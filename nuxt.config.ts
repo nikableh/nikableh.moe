d3348171263c975d88ae16870882932a4f078c73
Nika <nika@nikableh.moe>
2025-12-21 22:04:45 +0300

    Remove @vite-pwa/nuxt
    I just don't need it and it seems to affect Lighthouse too much by
    loading manifest.webmanifest.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
-import type { ManifestOptions } from 'vite-plugin-pwa'
-
-const appManifest: Partial<ManifestOptions> = {
-  name: "nikableh.moe",
-  short_name: "nikableh.moe",
-  description: "nikableh's website",
-  lang: "en",
-  start_url: "/",
-  id: "/",
-  orientation: "any",
-  background_color: "#171717",
-  display: "standalone",
-  theme_color: "#171717",
-  icons: [
-    {
-      "src": "/nikableh.png",
-      "sizes": "960x960",
-      "type": "image/png"
-    },
-  ]
-};
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
 
-  modules: ['@nuxt/image', '@nuxt/fonts', '@vite-pwa/nuxt'],
+  modules: ['@nuxt/image', '@nuxt/fonts'],
 
   runtimeConfig: {
     public: undefined
@@ -39,7 +18,6 @@ export default defineNuxtConfig({
     ],
   },
 
-
   fonts: {
     families: [
       { name: 'Maple Mono', src: '/fonts/MapleMonoNL-Regular.woff2' }
@@ -49,30 +27,19 @@ export default defineNuxtConfig({
     },
   },
 
-  pwa: {
-    selfDestroying: true,
-    devOptions: {
-      enabled: true
-    },
-    manifest: appManifest,
-  },
-
   app: {
     head: {
-      title: appManifest.name,
-      charset: 'utf-8',
+      title: "nikableh.moe",
+      charset: 'UTF-8',
       viewport: 'width=device-width, initial-scale=1, user-scalable=yes',
       htmlAttrs: {
         lang: 'en',
         dir: 'ltr',
       },
       meta: [
-        { name: "description", content: appManifest.description },
-        { name: "theme-color", content: appManifest.theme_color },
+        { name: "description", content: "nikableh's website" },
+        { name: "theme-color", content: "#171717" },
       ],
-      link: [
-        { rel: 'manifest', href: '/manifest.webmanifest' },
-      ]
     }
   },
 })
