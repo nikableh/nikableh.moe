75bf2ceec3d1d029d33046abaa2e5ce7b45ccd81
Nika <nika@nikableh.moe>
2025-12-21 20:21:49 +0300

    Use <NuxtImg> instead of <img>

--- app/components/Badge88x31.vue
+++ app/components/Badge88x31.vue
@@ -4,28 +4,18 @@ const props = defineProps<{
     image: string;
     alt: string;
 }>();
-
-const fallbackImage = "/88x31-unknown.png";
-
-const imgSrc = ref(props.image);
-
-watch(
-    () => props.image,
-    (newVal) => {
-        imgSrc.value = newVal;
-    }
-);
-
-function handleImageError(event: Event) {
-    const img = event.target as HTMLImageElement;
-    img.src = fallbackImage;
-}
 </script>
 
 <template>
     <ClientOnly>
         <NuxtLink :to="props.website" class="select-none inline-block m-1">
-            <img :src="imgSrc" :alt="props.alt" @error="handleImageError" />
+            <NuxtImg
+                width="88"
+                height="31"
+                :src="image"
+                :alt="props.alt"
+                placeholder="/88x31-unknown.png"
+            />
         </NuxtLink>
     </ClientOnly>
 </template>
