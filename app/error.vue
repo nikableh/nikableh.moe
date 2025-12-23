65c143cc94d5fbcc2cec92b5057178015a6380cd
Nika <nika@nikableh.moe>
2025-12-23 16:52:38 +0300

    Add error page

---
+++ app/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps<{ error: NuxtError }>()
+</script>
+
+<template>
+  <div>
+    <h1>{{ error.statusCode }}</h1>
+    <p>OOPSIE WOOPSIE!! Uwu you made a fucky wucky!! A wittle fucko boingo!</p>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
