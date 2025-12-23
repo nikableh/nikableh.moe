c10965ec67fe32b04f6111c262ea0f0b1b0528d3
Nika <nika@nikableh.moe>
2025-12-23 16:49:00 +0300

    Make bodily color global

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="bg-yetdarker h-full text-bodily flex flex-col items-center">
+    <section class="bg-yetdarker h-full flex flex-col items-center">
         <WipRibbon />
         <div class="flex flex-col items-center md:w-2xl">
             <main class="p-5 min-h-screen gap-5 flex flex-col items-center">
