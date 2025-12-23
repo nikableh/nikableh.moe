a8f330064b1df4a51993a36b30103b9e11173414
Nika <nika@nikableh.moe>
2025-12-24 01:54:40 +0300

    Add max-w-2xl for the default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="flex flex-col items-center w-full">
-        <div class="flex flex-col items-center w-full">
+        <div class="flex flex-col items-center w-full max-w-2xl">
             <main class="p-5 min-h-screen w-full max-w-2xl gap-5 flex flex-col items-center">
                 <slot />
             </main>
