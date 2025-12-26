1de7c6b090300b69889a7a563bbc14ab7674d9f6
Nika <nika@nikableh.moe>
2025-12-26 18:06:12 +0300

    Adapt / for a VERY small screen

--- app/components/MeSummaryTable.vue
+++ app/components/MeSummaryTable.vue
@@ -3,35 +3,41 @@ const { age } = useBirthday();
 </script>
 
 <template>
-    <div class="grid grid-cols-[auto_auto] gap-x-7 w-max">
-        <span>Location:</span>
-        <p>Moscow, Russia</p>
+    <div
+        class="w-full overflow-x-auto min-w-0 flex justify-center items-center"
+    >
+        <div class="grid grid-cols-[auto_auto] gap-x-7 w-max whitespace-pre overflow-x-auto">
+            <span>Location:</span>
+            <p>Moscow, Russia</p>
 
-        <span>Gender:</span>
-        <p>she/her 🏳️‍⚧️</p>
+            <span>Gender:</span>
+            <p>she/her 🏳️‍⚧️</p>
 
-        <span>Age:</span>
-        <p>{{ age }} y.o.</p>
+            <span>Age:</span>
+            <p>{{ age }} y.o.</p>
 
-        <span>Telegram:</span>
-        <p>
-            <NuxtLink to="https://t.me/nikableh" class="text-link"
-                >@nikableh</NuxtLink
-            >
-        </p>
+            <span>Telegram:</span>
+            <p>
+                <NuxtLink to="https://t.me/nikableh" class="text-link"
+                    >@nikableh</NuxtLink
+                >
+            </p>
 
-        <span>Github:</span>
-        <p>
-            <NuxtLink to="https://github.com/nikableh" class="text-link"
-                >@nikableh</NuxtLink
-            >
-        </p>
+            <span>Github:</span>
+            <p>
+                <NuxtLink to="https://github.com/nikableh" class="text-link"
+                    >@nikableh</NuxtLink
+                >
+            </p>
 
-        <span>Email:</span>
-        <p>
-            <NuxtLink to="mailto:nika@nikableh.moe" class="text-link break-all"
-                >nika@nikableh.moe</NuxtLink
-            >
-        </p>
+            <span>Email:</span>
+            <p>
+                <NuxtLink
+                    to="mailto:nika@nikableh.moe"
+                    class="text-link break-all"
+                    >nika@nikableh.moe</NuxtLink
+                >
+            </p>
+        </div>
     </div>
 </template>
