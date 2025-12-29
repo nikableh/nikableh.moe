a0ea54036881c4be8fcb027239c4c03265c58ec7
Nika <nika@nikableh.moe>
2025-12-29 14:24:32 +0300

    Fix navigation problem

---
+++ app/pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const { data } = await useAsyncData(() =>
+    queryCollection("content").path("/").first()
+);
+
+if (!data.value) {
+    throw createError({
+        status: 404,
+        statusText: "Page Not Found, stupid",
+    });
+}
+
+useSeoMeta({
+    title: data.value?.title,
+    description: data.value?.description,
+});
+</script>
+
+<template>
+    <article class="w-full  ">
+        <ContentRenderer :value="data!" />
+    </article>
+</template>
