93b781cd0272a8c62956ca082cd9038bcaa206dd
Nika <nika@nikableh.moe>
2026-01-11 20:38:15 +0300

    Remove NavBar for now

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
     <section class="flex flex-col items-center w-full">
-        <NavBar />
         <div class="flex flex-col items-center w-full max-w-2xl">
             <main
                 class="p-5 min-h-screen w-full max-w-2xl gap-5 flex flex-col items-center"
